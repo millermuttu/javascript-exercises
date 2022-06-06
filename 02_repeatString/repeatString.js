d258a54503874809516e67f2eee8a399543c3fba
millermuttu <flyingmuttus1995@gmail.com>
2022-06-06 23:26:26 +0530

    first two test completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,22 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let repeat_str = '';
+    if (num===0)
+    {
+        return repeat_str;
+    }
+    else if (num>0)
+    {
+        for (let i=0; i<num; i++)
+        {
+            repeat_str += str;
+        }
+    }
+    else
+    {
+        return "ERROR"
+    }
+
+    return repeat_str
 
 };
 
