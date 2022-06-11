823b922c0f170ea7896cfd72572502aac1102be9
millermuttu <flyingmuttus1995@gmail.com>
2022-06-11 23:41:19 +0530

    palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    var re = /[\W_]/g;
+    var lowRegStr = str.toLowerCase().replace(re, '');
+
+    var reverse = lowRegStr.split('').reverse().join('');
+
+    return lowRegStr===reverse;
 
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+// var a = "man a";
+// console.log(a.split(""))
